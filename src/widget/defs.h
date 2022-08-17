c4c2d9d6e1fd201a8b58b5126a7fc73526a55410
Paul Manias <paul@parasol.ws>
2022-08-17 22:27:00 +0100

    Removed deprecated code from the widget module

--- src/widget/defs.h
+++ src/widget/defs.h
@@ -1,36 +1,8 @@
 
-extern ERROR init_button(void);
-extern ERROR init_checkbox(void);
 extern ERROR init_clipboard(void);
-extern ERROR init_resize(void);
-extern ERROR init_combobox(void);
-extern ERROR init_input(void);
-extern ERROR init_scroll(void);
-extern ERROR init_scrollbar(void);
-extern ERROR init_tabfocus(void);
-extern ERROR init_text(void);
-extern ERROR init_view(void);
 
-extern void free_button(void);
-extern void free_checkbox(void);
 extern void free_clipboard(void);
-extern void free_combobox(void);
-extern void free_input(void);
-extern void free_resize(void);
-extern void free_scroll(void);
-extern void free_scrollbar(void);
-extern void free_tabfocus(void);
-extern void free_text(void);
-extern void free_view(void);
 
 extern struct DisplayBase *DisplayBase;
 extern struct SurfaceBase *SurfaceBase;
-extern struct FontBase *FontBase;
-extern struct KeyboardBase *KeyboardBase;
-extern struct VectorBase *VectorBase;
-extern OBJECTPTR modFont, modWidget, modDisplay, modSurface, modVector;
-extern char glDefaultFace[64];
-extern char glWindowFace[64];
-extern char glWidgetFace[64];
-extern char glLabelFace[64];
-extern LONG glMargin;
+extern OBJECTPTR modWidget, modDisplay, modSurface;
