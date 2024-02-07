863125c21e44e9728ea4bf5ad6545a0b75274dc3
Paul Manias <paul@parasol.ws>
2024-02-07 21:52:53 +0000

    [AGG] Removed agg_font_cache_manager.h

--- src/vector/agg/include/agg_basics.h
+++ src/vector/agg/include/agg_basics.h
@@ -19,27 +19,11 @@
 #else
 namespace agg
 {
-    // The policy of all AGG containers and memory allocation strategy
-    // in general is that no allocated data requires explicit construction.
-    // It means that the allocator can be really simple; you can even
-    // replace new/delete to malloc/free. The constructors and destructors
-    // won't be called in this case, however everything will remain working.
-    // The second argument of deallocate() is the size of the allocated
-    // block. You can use this information if you wish.
-    //------------------------------------------------------------pod_allocator
     template<class T> struct pod_allocator
     {
         static void deallocate(T* ptr) { delete [] ptr;      }
     };
 
-    // Single object allocator. It's also can be replaced with your custom
-    // allocator. The difference is that it can only allocate a single
-    // object and the constructor and destructor must be called.
-    // In AGG there is no need to allocate an array of objects with
-    // calling their constructors (only single ones). So that, if you
-    // replace these new/delete to malloc/free make sure that the in-place
-    // new is called and take care of calling the destructor too.
-    //------------------------------------------------------------obj_allocator
     template<class T> struct obj_allocator
     {
         static T*   allocate()         { return new T; }
