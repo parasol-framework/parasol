cdb94d39b9d8cef104e49028f352a20f9d861365
Paul Manias <paul@parasol.ws>
2024-02-21 20:51:43 +0000

    [SVG] Added additional mask tests

--- src/vector/vectors/group.cpp
+++ src/vector/vectors/group.cpp
@@ -10,6 +10,9 @@ If there is a need to adjust the container dimensions, use a @VectorViewport ins
 
 -END-
 
+NOTE: Groups can export a boundary if they have child paths, in which case all the paths are accummulated to form a
+single bounds area.  The calc_full_boundary() function can be used to do this.
+
 *********************************************************************************************************************/
 
 static ERROR init_group(void)
