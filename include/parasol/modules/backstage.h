5600b5e549a7c11191607dc3717f1fe62c91fde8
Paul Manias <paul@parasol.ws>
2025-07-31 21:24:25 +0100

    [Backstage] Added backstage include header

---
+++ include/parasol/modules/backstage.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Name:      backstage.h
+// Copyright: Paul Manias © 2025
+// Generator: idl-c
+
+#include <parasol/main.h>
+
+#define MODVERSION_BACKSTAGE (1)
+
+#include <parasol/network.h>
+
