6cb51059b3d7cd21784c9ad5c96c9f491ce6d56f
Paul Manias <31156549+paul-manias@users.noreply.github.com>
2025-11-11 11:22:06 +0000

    Add ?= compound if-empty assignment support

--- src/fluid/luajit-2.1/src/lj_lex.c
+++ src/fluid/luajit-2.1/src/lj_lex.c
@@ -407,6 +407,7 @@ static LexToken lex_scan(LexState *ls, TValue *tv)
       lex_next(ls);
       if (ls->c == '.') { lex_next(ls); return TK_safe_field; }
       if (ls->c == ':') { lex_next(ls); return TK_safe_method; }
+      if (ls->c == '=') { lex_next(ls); return TK_cif_empty; }
       if (ls->c != '?') return TK_if_empty; else { lex_next(ls); return TK_presence; }
     case '"':
     case '\'':
