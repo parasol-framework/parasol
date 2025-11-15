0f33e727a9a6ddfe7b6af4d395e9358f1d9a2d57
Paul Manias <paul@parasol.ws>
2025-11-15 01:23:55 +0000

    [Fluid] Cleaned up LuaJIT CPP files

--- src/fluid/luajit-2.1/src/lj_assert.cpp
+++ src/fluid/luajit-2.1/src/lj_assert.cpp
@@ -12,17 +12,17 @@
 
 #include "lj_obj.h"
 
-void lj_assert_fail(global_State *g, const char *file, int line,
-		    const char *func, const char *fmt, ...)
+void lj_assert_fail(global_State* g, const char* file, int line,
+   const char* func, const char* fmt, ...)
 {
-  va_list argp;
-  va_start(argp, fmt);
-  fprintf(stderr, "LuaJIT ASSERT %s:%d: %s: ", file, line, func);
-  vfprintf(stderr, fmt, argp);
-  fputc('\n', stderr);
-  va_end(argp);
-  UNUSED(g);  /* May be NULL. TODO: optionally dump state. */
-  abort();
+   va_list argp;
+   va_start(argp, fmt);
+   fprintf(stderr, "LuaJIT ASSERT %s:%d: %s: ", file, line, func);
+   vfprintf(stderr, fmt, argp);
+   fputc('\n', stderr);
+   va_end(argp);
+   UNUSED(g);  /* May be NULL. TODO: optionally dump state. */
+   abort();
 }
 
 #endif
