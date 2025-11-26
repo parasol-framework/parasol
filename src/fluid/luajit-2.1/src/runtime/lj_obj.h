4b9e84aa889dc9a010a9def03bceacb632e5c86d
Paul Manias <31156549+paul-manias@users.noreply.github.com>
2025-11-26 19:51:51 +0000

    Run close cleanup on explicit returns (#939)

--- src/fluid/luajit-2.1/src/runtime/lj_obj.h
+++ src/fluid/luajit-2.1/src/runtime/lj_obj.h
@@ -551,7 +551,7 @@ enum {
 #endif
 
 #define MMDEF(_) \
-  _(index) _(newindex) _(gc) _(mode) _(eq) _(len) \
+  _(index) _(newindex) _(gc) _(close) _(mode) _(eq) _(len) \
   /* Only the above (fast) metamethods are negative cached (max. 8). */ \
   _(lt) _(le) _(concat) _(call) \
   /* The following must be in ORDER ARITH. */ \
