ea9a6a9ff6c808f7c33646de7ce27e4f33fa8d4f
Paul Manias <paul@parasol.ws>
2025-12-30 10:40:46 +0000

    [Fluid] Optimised reserved name checks

--- src/fluid/luajit-2.1/src/parser/ir_emitter/emit_call.cpp
+++ src/fluid/luajit-2.1/src/parser/ir_emitter/emit_call.cpp
@@ -3,6 +3,10 @@
 //
 // #included from ir_emitter.cpp
 
+constexpr auto HASH_ASSERT  = pf::strhash("assert");
+constexpr auto HASH_MSG     = pf::strhash("msg");
+constexpr auto HASH_INCLUDE = pf::strhash("include");
+
 //********************************************************************************************************************
 // Pipe expression: lhs |> rhs_call()
 // Prepends the LHS result(s) as argument(s) to the RHS function call.
@@ -188,34 +192,24 @@ ParserResult<ExpDesc> IrEmitter::emit_call_expr(const CallExprPayload &Payload)
    // a way to disable them if tracing/profiling is enabled.
 
    if (const auto *direct = std::get_if<DirectCallTarget>(&Payload.target)) {
-      if (direct->callable) {
-         // Initialise interned strings for comparison (thread-safe static initialisation)
-         static GCstr *assert_str = nullptr;
-         static GCstr *msg_str = nullptr;
-         static GCstr *include_str = nullptr;
-         if (not assert_str) assert_str = lj_str_newlit(this->lex_state.L, "assert");
-         if (not msg_str) msg_str = lj_str_newlit(this->lex_state.L, "msg");
-         if (not include_str) include_str = lj_str_newlit(this->lex_state.L, "include");
+      if (direct->callable and direct->callable->kind IS AstNodeKind::IdentifierExpr) {
+         const auto *name_ref = std::get_if<NameRef>(&direct->callable->data);
+         if (name_ref and name_ref->identifier.symbol) {
+            GCstr *func_name = name_ref->identifier.symbol;
 
-         if (direct->callable->kind IS AstNodeKind::IdentifierExpr) {
-            const auto *name_ref = std::get_if<NameRef>(&direct->callable->data);
-            if (name_ref and name_ref->identifier.symbol) {
-               GCstr *func_name = name_ref->identifier.symbol;
-
-               if (func_name IS assert_str) this->optimise_assert(const_cast<ExprNodeList&>(Payload.arguments));
-               else if ((func_name IS msg_str) and not glPrintMsg) {
-                  // msg() is eliminated entirely when debug messaging is disabled at compile time.
-                  return ParserResult<ExpDesc>::success(ExpDesc(ExpKind::Void));
-               }
-               else if (func_name IS include_str) {
-                  // Intercept include('module_name') to pre-load constants at parse time
-                  if (not Payload.arguments.empty() and
-                      Payload.arguments[0]->kind IS AstNodeKind::LiteralExpr) {
-                     const auto *lit = std::get_if<LiteralValue>(&Payload.arguments[0]->data);
-                     if (lit and lit->kind IS LiteralKind::String and lit->string_value) {
-                        std::string mod_name(strdata(lit->string_value), lit->string_value->len);
-                        load_include(this->lex_state.L->script, mod_name.c_str());
-                     }
+            if (func_name->hash IS HASH_ASSERT) this->optimise_assert(const_cast<ExprNodeList&>(Payload.arguments));
+            else if ((func_name->hash IS HASH_MSG) and not glPrintMsg) {
+               // msg() is eliminated entirely when debug messaging is disabled at compile time.
+               return ParserResult<ExpDesc>::success(ExpDesc(ExpKind::Void));
+            }
+            else if (func_name->hash IS HASH_INCLUDE) {
+               // Intercept include('module_name') to pre-load constants at parse time
+               if (not Payload.arguments.empty() and
+                   Payload.arguments[0]->kind IS AstNodeKind::LiteralExpr) {
+                  const auto *lit = std::get_if<LiteralValue>(&Payload.arguments[0]->data);
+                  if (lit and lit->kind IS LiteralKind::String and lit->string_value) {
+                     std::string mod_name(strdata(lit->string_value), lit->string_value->len);
+                     load_include(this->lex_state.L->script, mod_name.c_str());
                   }
                }
             }
