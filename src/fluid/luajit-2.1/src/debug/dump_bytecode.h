0941ee47d8619142efbe5540ac43a94e89300c1c
Paul Manias <31156549+paul-manias@users.noreply.github.com>
2026-01-19 16:00:44 +0000

    [Fluid] Added file output to bytecode dumps (#1078)

--- src/fluid/luajit-2.1/src/debug/dump_bytecode.h
+++ src/fluid/luajit-2.1/src/debug/dump_bytecode.h
@@ -8,4 +8,4 @@
 using BytecodeLogger = std::function<void(std::string_view, void *)>;
 
 extern void dump_bytecode(FuncState &fs);
-extern void trace_proto_bytecode(GCproto *Proto, BytecodeLogger Logger, void *Meta, bool Verbose = false, int Indent = 0);
+extern void trace_proto_bytecode(lua_State *L, GCproto *Proto, BytecodeLogger Logger, void *Meta, bool Verbose = false, int Indent = 0);
